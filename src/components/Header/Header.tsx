4ced5fc8ecb676551934c575777890a087805abb
denis <devorlich@gmail.com>
2025-10-08 15:44:27 +0300

    feat: Header New

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,47 @@
 import { FC } from 'react';
 import './Header.scss';
 
-const Header: FC = () => {
+type HashTags = 'Review' | 'Экстрим' |'Легенды' | 'Зож и туризм' | 'Позірк' | 'Треш';
 
+interface NewsData {
+    hashTag: HashTags;
+    data: string
+}
+
+const HeaderPage: FC<{title: string}> = ({title}) => {
+    return <span>{title}</span>
+}
+
+const HeaderNew: FC<NewsData> = ({hashTag, data}) => {
+    return (
+        <div className='header_new'>
+            <span>#{hashTag}</span>
+            <span>{data}</span>
+        </div>
+    );
+}
+
+const Header: FC = () => {
+    const headerPages: HashTags[] = ['Review', 'Экстрим', 'Легенды', 'Зож и туризм', 'Позірк', 'Треш'];
+    const newData: NewsData = {
+        hashTag: 'Экстрим',
+        data: 'Юрий Поклад: «Динамо» как бы начинает работать заново'
+    }
 
     return (
         <header>
-            <div className='container'>
+            <div className="header_info">
                 <div className='header_line'>
                     <span className='header_logo'>
                         <i>
                             <span>sport</span>hub
                         </i>
                     </span>
-                    <div style={{display: 'flex', alignItems: 'center'}}>
+                    <div className='header_right_elements' >
                         <div className='header_pages'>
-                            <span>Review</span>
-                            <span>Экстрим</span>
-                            <span>Легенды</span>
-                            <span>зож и туризм</span>
-                            <span>Позірк</span>
-                            <span>Треш</span>
+                            {headerPages.map((titles, index) => {
+                                return <HeaderPage key={index} title={titles} />
+                            })}
                         </div>
                         <button className='header_aside_button'>
                             <div></div>
@@ -28,7 +49,14 @@ const Header: FC = () => {
                         </button>
                     </div>
                 </div>
+                <HeaderNew data={newData.data} hashTag={newData.hashTag} />
+                <div className="header_submenu">
+                    
+                </div>
             </div>
+            <div className='header_top_gradient'></div>
+            <div className='header_bottom_gradient'></div>
+            <div className='header_darker'></div>
         </header>
     ); 
 }
