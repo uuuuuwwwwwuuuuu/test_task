454ea5a23cf82bd610d284d137e913c353d8b3c1
denis <devorlich@gmail.com>
2025-10-09 14:09:33 +0300

    feat: adapted big new 2

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -11,6 +11,7 @@ const App = () => {
             <News />
             <BigNew isHashTag={true} imgName='big-new' titleOrHashTag='болельщикам' info='Юрий Поклад: «Динамо» как бы начинает работать заново' />
             <FeedBack />
+            <BigNew isHashTag={false} imgName='big-new-2' titleOrHashTag='Экстрим' info='Интервью с Юрием Покладом. Полный список всех медалей Беларуси на Европейских играх.' />
         </div>
     );
 };
--- src/components/bigNew/BigNew.tsx
+++ src/components/bigNew/BigNew.tsx
@@ -1,5 +1,6 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import './BigNew.scss';
+import useScreenSize from '../../dtectScreenSIze';
 
 interface BigNewProps {
     isHashTag: boolean;
@@ -8,10 +9,51 @@ interface BigNewProps {
     info: string
 }
 
+interface CalcOffsetOptions {
+    currentScreenSize: number
+    options: {
+        maxScreenSize: number;
+        minScreenSize: number;
+        maxOffsetValue: number;
+        minOffsetValue: number;
+    }
+
+}
+
+const calcOffset = (params: CalcOffsetOptions) => {
+    const {maxOffsetValue, maxScreenSize, minOffsetValue, minScreenSize} = params.options
+    const angularСoeficent = (maxOffsetValue - minOffsetValue) / (maxScreenSize - minScreenSize);
+    const offsetValue = maxOffsetValue - angularСoeficent * maxScreenSize
+    return angularСoeficent * params.currentScreenSize + offsetValue;
+
+}
+
 const BigNew: FC<BigNewProps> = ({isHashTag, imgName, titleOrHashTag, info}) => {
+    const screenWidth = useScreenSize().width;
+
+    const [offset, setOffset] = useState(125);
+
+    useEffect(() => {
+        if (screenWidth >= 1920) {
+            setOffset(125);
+        } else if (screenWidth <= 1920 && screenWidth >= 300) {
+            setOffset(calcOffset({
+                currentScreenSize: screenWidth,
+                options: {
+                    maxScreenSize: 1920,
+                    minScreenSize: 400,
+                    maxOffsetValue: 125,
+                    minOffsetValue: 0
+                }
+            }));
+        } else {
+            setOffset(0);
+        }
+    }, [screenWidth])
+
     return (
         <div className='big_new' style={{backgroundImage: `url(${process.env.PUBLIC_URL}/img/${imgName}.png)`}} >
-            <div className='big_new_text' style={{width: !isHashTag ? 400 : '', right: !isHashTag ? 155 : 0}}>
+            <div className='big_new_text' style={{width: !isHashTag ? 400 : '', right: !isHashTag ? offset : 0}}>
                 {
                     isHashTag ? <span className='big_new_hash_tag'>#{titleOrHashTag}</span> : <span className='big_new_title'>{titleOrHashTag}</span>
                 }
